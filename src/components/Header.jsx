3652662530b2537efe491c4ee7c8e722d4a908d0
LOVKUSH <lovkushyadav9888@gmail.com>
2023-05-15 22:29:24 +0530

    ProductList creation

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='flex shopping-card'>
+      <div>Shopping cart</div>
+      <div>Cart
+      <sup> {'3'} </sup>
+      </div>
+    </div>
+  )
+}
+
+export default Header;
